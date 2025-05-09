3b9129fed47576e8cccd3a56525e50cd15e2dff4
rejo132 <enoch.luke1@student.moringaschool.com>
2025-05-09 20:58:01 +0300

    completes lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 class Dog:
-    # Class body goes here
-
-    #Instance method definition
-    pass
+    def __init__(self):
+        pass
+    
+    def bark(self):
+        print("Woof!")
+    
+    def sit(self):
+        print("The dog is sitting.")
--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 class Person:
-    # Class body goes here
-
-    #Instance method definition
-    pass
+    def __init__(self):
+        pass
+    
+    def talk(self):
+        print("Hello World!")
+    
+    def walk(self):
+        print("The person is walking.")
